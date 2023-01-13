417d323e2436bf94bbc1c24555dbebe708bb665b
Yuri Lavrentiev <yuri.lavrentiev.js@gmail.com>
2023-01-13 11:53:38 +0200

    quota token bunus info added

--- js/calculator.js
+++ js/calculator.js
@@ -1,35 +1,64 @@
-const buttonCalculate = document.getElementById('btnCalc');
-const buttonBallance = document.getElementById('btnBall')
-const input = document.getElementById('input');
-const contentContainer = document.getElementById('info');
-const nftInfoContainer = document.getElementById('nft')
+const buttonCalculate = document.getElementById("btnCalc");
+const buttonBallance = document.getElementById("btnBall");
+const input = document.getElementById("input");
+const contentContainer = document.getElementById("info");
+const nftInfoContainer = document.getElementById("nft");
 
-
-buttonCalculate.addEventListener('click', () => { calculate(input.value) });
-buttonBallance.addEventListener('click', () => { showBallance(input.value) });
+buttonCalculate.addEventListener("click", () => {
+  calculate(input.value);
+});
+buttonBallance.addEventListener("click", () => {
+  showBallance(input.value);
+});
 
 async function calculate(address) {
   const regex = /^[\w]{42}$/i;
   if (regex.test(address)) {
-    input.className = 'valid';
-    const data = await fetch("https://api2.quota.team/data/bonusFriday.json").then((responce) => responce.json());
-    
-    contentContainer.innerText = `Your bonuses are:\nDAIQUIRI: ${data.DAIQUIRI[address] ? data.DAIQUIRI[address].toFixed(4) : 0}\nAAPLQQX: ${data.AAPLQQX[address] ? data.AAPLQQX[address].toFixed(4) : 0}\nBARREL: ${data.BARREL[address] ? data.BARREL[address].toFixed(4) : 0}\nYELLOW: ${data.YELLOW[address] ? data.YELLOW[address].toFixed(4) : 0}\nMICROB: ${data.MICROB[address] ? data.MICROB[address].toFixed(4) : 0}\nISLAND: ${data.ISLAND[address] ? data.ISLAND[address].toFixed(4) : 0}\nUBERQQX: ${data.UBERQQX[address] ? data.UBERQQX[address].toFixed(4) : 0}\nNTDOYQX: ${data.NTDOYQX[address] ? data.NTDOYQX[address].toFixed(4) : 0}\nBIGMAC: ${data.BIGMAC[address] ? data.BIGMAC[address].toFixed(4) : 0}`
-    nftInfoContainer.innerText = '';
+    input.className = "valid";
+    const data = await fetch(
+      "https://api2.quota.team/data/bonusFriday.json"
+    ).then((responce) => responce.json());
+
+    contentContainer.innerText = `Your bonuses are:\nDAIQUIRI: ${
+      data.DAIQUIRI[address] ? data.DAIQUIRI[address].toFixed(4) : 0
+    }\nAAPLQQX: ${
+      data.AAPLQQX[address] ? data.AAPLQQX[address].toFixed(4) : 0
+    }\nBARREL: ${
+      data.BARREL[address] ? data.BARREL[address].toFixed(4) : 0
+    }\nYELLOW: ${
+      data.YELLOW[address] ? data.YELLOW[address].toFixed(4) : 0
+    }\nMICROB: ${
+      data.MICROB[address] ? data.MICROB[address].toFixed(4) : 0
+    }\nISLAND: ${
+      data.ISLAND[address] ? data.ISLAND[address].toFixed(4) : 0
+    }\nUBERQQX: ${
+      data.UBERQQX[address] ? data.UBERQQX[address].toFixed(4) : 0
+    }\nNTDOYQX: ${
+      data.NTDOYQX[address] ? data.NTDOYQX[address].toFixed(4) : 0
+    }\nBIGMAC: ${
+      data.BIGMAC[address] ? data.BIGMAC[address].toFixed(4) : 0
+    }\nQUOTA: ${data.QUOTA[address] ? data.QUOTA[address].toFixed(4) : 0}`;
+    nftInfoContainer.innerText = "";
   } else {
-    input.className = 'invalid';
-    contentContainer.innerText = 'You entered invalid wallet address';
-    nftInfoContainer.innerText = '';
+    input.className = "invalid";
+    contentContainer.innerText = "You entered invalid wallet address";
+    nftInfoContainer.innerText = "";
   }
 }
 
 async function showBallance(address) {
   const regex = /^[\w]{42}$/i;
   if (regex.test(address)) {
-    input.className = 'valid';
-    const ballanceData = await fetch(`https://explorer-api.minter.network/api/v2/addresses/${address}`).then((responce) => responce.json());
-    const nftOwners = await fetch('https://api2.quota.team/data/nftOwners.json').then((responce) => responce.json());
-    const totalWalletBallance = await fetch('https://api2.quota.team/data/coins/TORTUGA.json').then((responce) => responce.json());
+    input.className = "valid";
+    const ballanceData = await fetch(
+      `https://explorer-api.minter.network/api/v2/addresses/${address}`
+    ).then((responce) => responce.json());
+    const nftOwners = await fetch(
+      "https://api2.quota.team/data/nftOwners.json"
+    ).then((responce) => responce.json());
+    const totalWalletBallance = await fetch(
+      "https://api2.quota.team/data/coins/TORTUGA.json"
+    ).then((responce) => responce.json());
     let nftArray = [];
     for (key in nftOwners) {
       if (nftOwners[key] === address) {
@@ -40,23 +69,35 @@ async function showBallance(address) {
     Promise.all(result)
       .then((values) => {
         let nftTotal = 0;
-        values.forEach((value) => nftTotal += +value);
+        values.forEach((value) => (nftTotal += +value));
         return nftTotal;
       })
-      .then((nftTotal) => nftInfoContainer.innerText = `Total ballance of all NFT's: \n${nftTotal ? nftTotal.toFixed(4) : 0} Tortuga`)
-        const ballanceArray = [...ballanceData.data.balances].filter((element) => element.coin.id === 3673);
-        contentContainer.innerText = `Current Tortuga ballance: ${totalWalletBallance[address] ? totalWalletBallance[address].toFixed(4) : 0}\nYou have ${nftArray.length} NFT's linked to your wallet`;
-
+      .then(
+        (nftTotal) =>
+          (nftInfoContainer.innerText = `Total ballance of all NFT's: \n${
+            nftTotal ? nftTotal.toFixed(4) : 0
+          } Tortuga`)
+      );
+    const ballanceArray = [...ballanceData.data.balances].filter(
+      (element) => element.coin.id === 3673
+    );
+    contentContainer.innerText = `Current Tortuga ballance: ${
+      totalWalletBallance[address] ? totalWalletBallance[address].toFixed(4) : 0
+    }\nYou have ${nftArray.length} NFT's linked to your wallet`;
   } else {
-    input.className = 'invalid';
-    contentContainer.innerText = 'You entered invalid wallet address';
-    nftInfoContainer.innerText = '';
+    input.className = "invalid";
+    contentContainer.innerText = "You entered invalid wallet address";
+    nftInfoContainer.innerText = "";
   }
 }
 
 async function getNFTBallance(address) {
   let nftTotal;
-  const nftBallance = await fetch(`https://explorer-api.minter.network/api/v2/addresses/${address}`).then((response) => response.json());
-  const ballanceArray = [...nftBallance.data.balances].filter((element) => element.coin.id === 3673);
-  return nftTotal = Number(ballanceArray[0].amount);
+  const nftBallance = await fetch(
+    `https://explorer-api.minter.network/api/v2/addresses/${address}`
+  ).then((response) => response.json());
+  const ballanceArray = [...nftBallance.data.balances].filter(
+    (element) => element.coin.id === 3673
+  );
+  return (nftTotal = Number(ballanceArray[0].amount));
 }
